f65bc18566068ab7c5f0bc25d5c0b260aa600d69
Jesus-Esquivel <a.22308051281176@cbtis128.edu.mx>
2024-09-11 07:48:59 -0600

    Add files via upload

---
+++ mi_conjunto.py
@@ -0,0 +1,6 @@
+print("Ejemplo conjuntos set")
+tipodeportes={"Futbol","Basquetbol","Tenis","Box"}
+print(tipodeportes)
+print("Lista elementos del conjunto del conjunto tipodeportes loop for")
+for Esquivel in tipodeportes:
+    print(Esquivel)
---
+++ mi_diccionario.py
@@ -0,0 +1,10 @@
+print("Ejemolo de Diccionarios")
+thisadict={
+    "Marca":"Corvette",
+    "Modelo":"ZR1",
+    "Año":"2018",
+}
+print(thisadict)
+print("for")
+for Esquivel in thisadict:
+    print(thisadict[Esquivel])
